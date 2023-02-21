d70af9c287e44231493fcfca53205d7fc27ab5a7
sansth1010 <sansth22@gmail.com>
2023-02-21 00:11:04 -0600

    removed enrichment logic from dcat ap output plugin

--- src/dcat-ap/index.ts
+++ src/dcat-ap/index.ts
@@ -1,52 +1,43 @@
-import { IDomainEntry, IModel } from '@esri/hub-common';
-import { defaultCalculatedFields, defaultRequiredFields, getDcatDataset } from './dcat-dataset';
-import { DatasetFormatTemplate, formatDcatCatalog, formatDcatDataset, mergeWithDefaultFormatTemplate } from './dcat-formatters';
 import { FeedFormatterStream } from './feed-formatter-stream';
-import { listDependencies } from 'adlib';
+import { TransformsList } from 'adlib';
+import { compileDcatFeedEntry } from './compile-dcat-feed';
 
-interface IDcatAPOptions {
-  siteUrl: string, // can't currently derive from siteModel because some site item urls are out of sync
-  siteModel: IModel;
-  domainRecord: IDomainEntry,
-  orgBaseUrl: string;
-  customFormatTemplate?: DatasetFormatTemplate
-}
+export function getDataStreamDcatAp201(feedTemplate: any, feedTemplateTransforms: TransformsList) {
+  const catalogStr = JSON.stringify({
+    '@context': {
+      dcat: 'http://www.w3.org/ns/dcat#',
+      dct: 'http://purl.org/dc/terms/',
+      foaf: 'http://xmlns.com/foaf/0.1/',
+      vcard: 'http://www.w3.org/2006/vcard/ns#',
+      ftype: 'http://publications.europa.eu/resource/authority/file-type/',
+      lang: 'http://publications.europa.eu/resource/authority/language/',
+    },
+    ...feedTemplate.header
+  }, null, '\t');
 
-export function getDataStreamDcatAp201(options: IDcatAPOptions) {
-  const catalogStr = formatDcatCatalog({ 
-    siteItem: options.siteModel.item,
-    domainRecord: options.domainRecord,
-    orgBaseUrl: options.orgBaseUrl,
-  });
-
-  // lop off the "\n}"
   const header = `${catalogStr.substr(
     0,
     catalogStr.length - 2,
   )},\n\t"dcat:dataset": [\n`;
 
   const footer = '\n\t]\n}';
-  
-  const datasetFormatTemplate = mergeWithDefaultFormatTemplate(options.customFormatTemplate);
+
   const formatFn = (chunk) => {
-    const dcatDataset = getDcatDataset(
-      chunk, 
-      options.orgBaseUrl, 
-      options.domainRecord.orgTitle, 
-      options.siteUrl,
-      options.siteModel
-    );
-    return formatDcatDataset(dcatDataset, datasetFormatTemplate);
+    return compileDcatFeedEntry(chunk, objectWithoutKeys(feedTemplate, ['header']), feedTemplateTransforms);
   };
 
   return {
-    dcatStream: new FeedFormatterStream(header, footer, ',\n', formatFn),
-    dependencies: Array.from(
-      new Set([
-      ...defaultRequiredFields,
-      ...listDependencies(datasetFormatTemplate)
-          .filter(dependency => !defaultCalculatedFields.includes(dependency))
-      ])
-    )
+    dcatStream: new FeedFormatterStream(header, footer, ',\n', formatFn)
   };
+}
+
+/**
+ * fast approach to remove keys from an object
+ * (from babel transplier)
+ */
+function objectWithoutKeys(obj, keys) {
+  return Object.keys(obj).reduce((newObject, key) => {
+    if (keys.indexOf(key) === -1) newObject[key] = obj[key];
+    return newObject;
+  }, {});
 }
--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,10 @@
 import { Request, Response } from 'express';
 import * as _ from 'lodash';
 
-import {
-  getHubApiUrl,
-  getPortalApiUrl,
-  getSiteById,
-  hubApiRequest,
-  IDomainEntry,
-  IHubRequestOptions,
-  lookupDomain,
-  RemoteServerError,
-} from '@esri/hub-common';
-import { IContentSearchRequest, SortDirection } from '@esri/hub-search';
-
 import { version } from '../package.json';
 import { getDataStreamDcatAp201 } from './dcat-ap';
-import { portalUrl } from './config';
-
-let env: 'prod' | 'qa' | 'dev' = 'prod';
-if (/devext\.|mapsdev\./.test(portalUrl)) {
-  env = 'dev';
-} else if (/qaext\.|mapsqa\./.test(portalUrl)) {
-  env = 'qa';
-}
-
-/**
-  * This function converts adlib'ed fields from the specified catalog into valid API fields used
-  * to query the API for catalog content.
-  * 
-  * For fields that specify a path hierarchy using the || operator,
-  * process each field as an API field EXCEPT for the last one.
-  * The last field is interpreted as EITHER a templated value (e.g. `"modifed")
-  * OR a literal value (e.g. "my literal value")
-  * See "Path Hierarchies and Defaults" at https://github.com/Esri/adlib
-  * 
-  * Because the last field can be interpreted as either, with no syntax to differentiate,
-  * the last field will be treated as a literal if it is not a valid Hub API field. As such,
-  * it is not converted to a Hub API field
-  * 
-  * @param dependencies - list of fields processed by adlib to use when building the catalog
-  * @returns - a list of valid Hub API fields
-*/
-async function getApiTermsFromDependencies (dependencies: string[]) {
-  if (!dependencies || !Array.isArray(dependencies)) return undefined;
-
-  // Only get valid Hub API fields if they are needed
-  const doesPathHierarchyExist = dependencies.filter(dep => dep.includes('||')).length;
-  const validApiFields: string[] = doesPathHierarchyExist ? await hubApiRequest('fields') : [];
-  const validApiFieldMap = validApiFields.reduce((fieldMap, field) => {
-    fieldMap[field] = true;
-    return fieldMap;
-  }, {});
-
-  return Array.from(new Set(_.flatten(dependencies.map(dep => {
-    // Dependency could indicate a hierarchial path (e.g. orgEmail || author)
-    if (dep.includes('||')) {
-      const providedSubDeps = dep.split('||').map(subDep => subDep.trim()).filter(subDep => !!subDep);
-      const returnedSubDeps = [];
-
-      // Assume all non-last fields are valid API fields
-      for (let i = 0; i < providedSubDeps.length - 1; i++) {
-        returnedSubDeps.push(providedSubDeps[i].split('.')[0]);
-      }
-
-      // Only push the last one if its a valid API field
-      if (validApiFieldMap[providedSubDeps[providedSubDeps.length - 1]]) {
-        returnedSubDeps.push(providedSubDeps[providedSubDeps.length - 1].split('.')[0]);
-      }
-
-      return returnedSubDeps;
-    }
-    return dep.split('.')[0];
-  }))));
-}
-
-/**
- * Sort field map that connects English versions of sort fields
- * to API sort field keys
- */
- const sortFieldMap = {
-  'Date Created': 'created',
-  'Date Modified': 'modified',
-};
+import { TransformsList } from 'adlib';
+import { DcatApError } from './dcat-ap/dcat-ap-error';
 
 export = class OutputDcatAp201 {
   static type = 'output';
@@ -100,155 +23,35 @@ export = class OutputDcatAp201 {
     res.set('Content-Type', 'application/json');
 
     try {
-      const { domainRecord, siteModel } = await this.fetchDomainAndSite(req.hostname);
 
-      // Use dcatConfig query param if provided, else default to site's config
-      let dcatConfig = typeof req.query.dcatConfig === 'string'
-        ? this.parseProvidedDcatConfig(req.query.dcatConfig as string)
-        : req.query.dcatConfig;
-    
-      if (!dcatConfig) {
-        dcatConfig = _.get(siteModel, 'data.feeds.dcatAP201');
+      const { res: { locals: { feedTemplate } }, app: { locals: { feedTemplateTransformsDcatAp } } }: {
+        res?: {
+          locals?: {
+            feedTemplate?: any
+          }
+        }
+        app: {
+          locals: {
+            feedTemplateTransformsDcatAp?: TransformsList
+          }
+        }
+      } = req;
+
+      if (!feedTemplate) {
+        throw new DcatApError('DCAT-AP 2.0.1 feed template is not provided.', 400);
       }
 
-      const orgBaseUrl = `https://${domainRecord.orgKey}.maps${
-        env === 'prod' ? '' : env
-      }.arcgis.com`;
-
-      // TODO: We only pass in hostname because some site item urls are out of sync, causing invalid urls for
-      // hubLandingPage and identifier. If we can resolve the syncing issues, we can omit hostname and just use
-      // the absolute url we get from getContentSiteUrls()
-
-      const { dcatStream, dependencies } = getDataStreamDcatAp201({
-        domainRecord,
-        siteUrl: req.hostname,
-        siteModel,
-        orgBaseUrl,
-        customFormatTemplate: dcatConfig
-      });
-
-      const apiTerms = await getApiTermsFromDependencies(dependencies);
-      
-      // Construct request to send, send empty if no id or catalog
-      const id = String(req.query.id || '');
-      const siteCatalog = _.get(siteModel, 'data.catalog');
-
-      if (!id && !siteCatalog) {
-        res.status(200).send({});
-        return;
-      }
-  
-      req.res.locals.searchRequest = id 
-        ? this.getDatasetSearchRequest({ id, fields: apiTerms })
-        : this.getCatalogSearchRequest({ req, catalog: siteCatalog, fields: apiTerms });
+      const { dcatStream } = getDataStreamDcatAp201(feedTemplate, feedTemplateTransformsDcatAp);
 
       const datasetStream = await this.getDatasetStream(req);
 
       datasetStream
         .pipe(dcatStream)
-        .pipe(res)
-        .on('error', (err: any) => {
-          res.status(500).send(this.getErrorResponse(err));
-        });
+        .pipe(res);
+
     } catch (err) {
-      res.status(err.status || 500).send(this.getErrorResponse(err));
+      res.status(err.statusCode).send(this.getErrorResponse(err));
     }
-  }
-
-  private async fetchDomainAndSite(hostname) {
-    const requestOptions = this.getRequestOptions(portalUrl);
-
-    try {
-      const domainRecord = (await lookupDomain(
-        hostname,
-        requestOptions,
-      )) as IDomainEntry;
-      const siteModel = await getSiteById(domainRecord.siteId, requestOptions);
-  
-      return { domainRecord, siteModel };
-    } catch (err) {
-
-      // Throw 404 if domain does not exist (first) or site is private (second)
-      if (err.message.includes(':: 404') || err.response?.error?.code === 403) {
-        throw new RemoteServerError(err.message, null, 404);
-      }
-      throw new RemoteServerError(err.message, null, 500);
-    }
-  }
-
-  private parseProvidedDcatConfig(dcatConfig: string) {
-    try {
-      return JSON.parse(dcatConfig);
-    } catch (err) {
-      return undefined;
-    }
-  }
-
-  private getRequestOptions(portalUrl: string): IHubRequestOptions {
-    return {
-      isPortal: false,
-      hubApiUrl: getHubApiUrl(portalUrl),
-      portal: getPortalApiUrl(portalUrl),
-      authentication: null,
-    };
-  }
-
-  private getDatasetSearchRequest(opts: {
-    id: string,
-    fields: string[]
-  }): IContentSearchRequest {
-    const searchRequest: IContentSearchRequest = {
-      filter: { id: opts.id },
-      options: {
-        portal: portalUrl,
-        fields: opts.fields ? opts.fields.join(',') : undefined
-      },
-    };
-    return searchRequest;
-
-  }
-
-  private getCatalogSearchRequest(opts: {
-    req: Request,
-    catalog: any,
-    fields: string[]
-  }): IContentSearchRequest {
-    const searchRequest: IContentSearchRequest = {
-      filter: {
-        group: opts.catalog.groups,
-        orgid: opts.catalog.orgId,
-      },
-      options: {
-        portal: portalUrl,
-        fields: opts.fields ? opts.fields.join(',') : undefined,
-      },
-    };
-
-    if (typeof _.get(opts, 'req.query.q') === 'string' && opts.req.query.q.length > 0) {
-      searchRequest.filter.terms = opts.req.query.q as string;
-    }
-
-    const sortOptions = this.getSortOptions(_.get(opts, 'req.query.sort', undefined));
-    if (sortOptions) {
-      searchRequest.options.sortField = sortOptions.sortField;
-      searchRequest.options.sortOrder = sortOptions.sortOrder;
-    }
-    return searchRequest;
-  }
-
-  private getSortOptions(sortQuery: string): { sortField?: string; sortOrder?: SortDirection } {
-    if (typeof sortQuery !== 'string' || !sortQuery.length) {
-      return undefined;
-    }
-
-    const sortOptions = sortQuery.split('|');
-    
-    const sortField = sortOptions.length > 1 ? sortOptions[1] : sortFieldMap[sortOptions[0]];
-    const sortOrder = sortOptions.length > 2 ? sortOptions[2] as SortDirection : SortDirection.desc;
-    return {
-      sortField,
-      sortOrder,
-    };
   }
 
   private async getDatasetStream(req: Request) {
@@ -256,9 +59,9 @@ export = class OutputDcatAp201 {
       return await this.model.pullStream(req);
     } catch (err) {
       if (err.status === 400) {
-        throw new RemoteServerError(err.message, null, 400);
+        throw new DcatApError(err.message, 400);
       }
-      throw new RemoteServerError(err.message, null, 500);
+      throw new DcatApError(err.message, 500);
     }
   }
 
